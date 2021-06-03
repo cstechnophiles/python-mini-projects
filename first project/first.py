a8e10c9a1ebdcf0df17c11974414fb94b0963bc1
deepjotsingh8837 <deepjotsingh8837@gmail.com>
2021-06-03 12:18:33 +0530

    aakash bad boi

--- first project/first.py
+++ first project/first.py
@@ -1,1 +1,27 @@
-print("hello")
+import turtle
+
+sc=turtle.Screen()
+
+sc.setup(600,600)
+
+spiral= turtle. Turtle()
+
+spiral.speed(10)
+
+sc.bgcolor("black")
+
+col=("yellow","blue","white","green")
+
+C=0
+
+for i in range(50): spiral.forward(i*10)
+
+spiral.right(144) spiral.color(col[c])
+
+If c==3:
+
+C=0
+
+else:
+
+C+=1
